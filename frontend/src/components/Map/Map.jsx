b22c4b8b2c61350b4f88a85f7d857c69597b545a
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-07-22 17:15:30 +0300

    change zoom (Map)

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer } from "react-leaflet"
 
 export default function Map() {
     return (
-        <MapContainer id="eventsMap" center={[37.9756, 23.7346]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer id="eventsMap" center={[37.9756, 23.7346]} zoom={12} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
