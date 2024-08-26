b8bf53e88a64520da26a621ab062dced25f9b910
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-08-26 21:19:32 +0200

    unique groupcodes per venueId util function

--- frontend/src/util/events.js
+++ frontend/src/util/events.js
@@ -3,23 +3,52 @@ export function find_unique_venues(data) {
 
     // Finds unique venueIds
 
-    const venueIdCount = new Map();
+    const venueIdCount = new Map()
 
     data.forEach(item => {
-        const venueId = item.venue.venue_id;
+        const venueId = item.venue.venue_id
         if (venueIdCount.has(venueId)) {
-            venueIdCount.set(venueId, venueIdCount.get(venueId) + 1);
+            venueIdCount.set(venueId, venueIdCount.get(venueId) + 1)
         } else {
-            venueIdCount.set(venueId, 1);
+            venueIdCount.set(venueId, 1)
         }
-    });
+    })
 
-    const uniqueVenueIds = [];
+    const uniqueVenueIds = []
     venueIdCount.forEach((count, venueId) => {
         if (count === 1) {
-            uniqueVenueIds.push(venueId);
+            uniqueVenueIds.push(venueId)
         }
-    });
+    })
 
-    return uniqueVenueIds;
+    return uniqueVenueIds
 }
+
+export function find_unique_groups_per_venue(data) {
+    const venueEventGroupMap = {}
+
+    data.forEach(item => {
+        const venueId = item.venue.venue_id
+        const eventGroupCode = item.group.event_group_code
+
+        if (!venueEventGroupMap[venueId]) {
+            venueEventGroupMap[venueId] = {}
+        }
+
+        if (!venueEventGroupMap[venueId][eventGroupCode]) {
+            venueEventGroupMap[venueId][eventGroupCode] = 0
+        }
+
+        venueEventGroupMap[venueId][eventGroupCode]++
+    })
+
+    const result = {}
+    for (const [venueId, eventGroupCodes] of Object.entries(venueEventGroupMap)) {
+        result[venueId] = Object.keys(eventGroupCodes).filter(
+            groupCode => eventGroupCodes[groupCode] === 1
+        )
+    }
+
+    return result
+}
+
