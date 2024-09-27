1a486f8c67c0d5ae3a59a56f83ffef80d7530c6b
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-09-27 12:54:58 +0300

    update scraper to select Greece

--- get_active_urls.py
+++ get_active_urls.py
@@ -4,7 +4,8 @@ import json
 import sys
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0',
+    'cookie': 'Country=GR'
 }
 
 # Get all event info from the main tickets page
